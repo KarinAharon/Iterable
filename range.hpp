7e75890098fb67c51a2ebb2edc10e82d69d60043
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-14 09:37:57 +0300

    Update range.hpp

--- range.hpp
+++ range.hpp
@@ -1,6 +1,5 @@
 #pragma once
-#include <iostream>
-//http://www.cplusplus.com/reference/iterator/iterator/
+#include <iostream>//http://www.cplusplus.com/reference/iterator/iterator/
 namespace itertools{
 
 template <typename R> 
@@ -13,24 +12,28 @@ public:
     
     class iterator {
     private:
-       R* iter;
+       R iter;
 
     public:
 
-       iterator(R* iter) : iter(iter){}
-       iterator& operator++() {++(*iter);return *this;}
-       iterator operator++(int) {iterator tmp(*this); operator++(); return tmp;}
-       bool operator==(const iterator &other) const {return *iter == *other.iter;}
-       bool operator!=(const iterator &other) const {return *iter != *other.iter;}
-       R& operator*() {return *iter;}
+       iterator(R iter) : iter(iter){}
+       auto& operator++() {++(iter);return *this;}
+       const iterator operator++(int) {
+         iterator tmp(*this); 
+         iter++;
+         return tmp;}
+       bool operator==(const iterator &other) const {return iter == other.iter;}
+       bool operator!=(const iterator &other) const {return iter != other.iter;}
+       auto& operator*() const {return iter;}
 
 
     };
     public:
     range(R _start, R _stop): start(_start), stop(_stop){};
-    auto begin() { return iterator(&start); }
-    auto end() { return iterator(&stop); }
+    auto begin() { return range<R>::iterator(start); }
+    auto end() { return range<R>::iterator(stop); }
 
     
 };
 }
+
