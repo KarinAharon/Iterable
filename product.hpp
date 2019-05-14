c10c3be29e20b23c7314929d7a794aaed8908c97
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-14 09:37:30 +0300

    Update product.hpp

--- product.hpp
+++ product.hpp
@@ -21,11 +21,11 @@ template <typename R, typename W> class iterator {
     public:
 
        iterator(R it1 , W it2):iter1(it1), iter2(it2){}
-       iterator<R,W>& operator++() {++(*iter1);return *this;}
+       iterator<R,W>& operator++() {++(iter1);return *this;}
        iterator<R,W>& operator++(int) {iterator tmp(*this); operator++(); return tmp;}
        bool operator==(const iterator<R,W> other) const {return iter1 == other.iter1;}
        bool operator!=(const iterator<R,W> other) const {return iter1 != other.iter1;}
-       decltype(*iter1) operator*() {return *iter1;}
+       auto& operator*() {return *iter1;}
        
     };
 
