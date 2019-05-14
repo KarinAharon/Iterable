f75a3b8e651e2a9436eaae75221d0e69d9fc2ff6
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-14 13:36:13 +0300

    Update range.hpp

--- range.hpp
+++ range.hpp
@@ -1,23 +1,23 @@
 #pragma once
 #include <sstream>
-#include <iostream>//http://www.cplusplus.com/reference/iterator/iterator/
+#include <iostream>
+//https://doc.qt.io/qt-5/qset-const-iterator.html#operator--gt
+//http://www.cplusplus.com/reference/iterator/iterator/
 namespace itertools{
 
 template <typename R> 
 class range{
+const R start;
+const R stop;
 
-R start;
-R stop;
-
-public:
-    
+public: 
     class iterator {
     private:
-       R iter;
+        R iter;
 
     public:
 
-       iterator(R iter) : iter(iter){}
+       iterator(const R &iter) : iter(iter){}
        auto& operator++() {++(iter);return *this;}
        const iterator operator++(int) {
          iterator tmp(*this); 
@@ -29,38 +29,16 @@ public:
 
 
     };
- 
-public:
-
-    class const_iterator{
-    private:
-    R const_iter;
-  
     public:
-    const_iterator(R const_iter) :const_iterator(&const_iter){}
-    auto& operator++() {++(const_iter);return *this;}
-    const const_iterator operator++(int) {
-         const_iterator tmp(*this); 
-         const_iter++;
-         return tmp;}
-    bool operator==(const const_iterator &other) const{return const_iter == other.const_iter;}
-    bool operator!=(const const_iterator &other) const{return const_iter != other.const_iter;}
-    auto& operator*() const {return const_iter;}
-
-};
-
-
-    public:
-    range(R _start, R _stop): start(_start), stop(_stop){};
+    range(R _start , R _stop): start(_start), stop(_stop){};
 
     iterator begin() const { return range<R>::iterator(start); }
-    iterator end() const { return range<R>::iterator(stop); }
-
-    const_iterator cbegin() const noexcept { range<R>::const_iterator(start); }
-    const_iterator cend() const noexcept { range<R>::const_iterator(stop); }
-
-    
+    iterator end() const { return range<R>::iterator(stop); }   
 };
 }
+
+
+
+
 
 
