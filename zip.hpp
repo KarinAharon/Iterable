42adb07cae045fca301f15cfe2fd82dfeccf93c7
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-14 13:38:25 +0300

    Update zip.hpp

--- zip.hpp
+++ zip.hpp
@@ -7,19 +7,19 @@ namespace itertools{
  template <typename T, typename R>class zip{
 
 private:
-T bin1;
-R bin2;
+const T bin1;
+const R bin2;
 
 
 template <typename U, typename W> class iterator {
 
     private:
-       U iter1;
-       W iter2;
+        U iter1;
+        W iter2;
 
      public:
         
-       iterator(U it1 , W it2):iter1(it1), iter2(it2){}
+       iterator(const U &it1 , const W &it2):iter1(it1), iter2(it2){}
        iterator<U,W>& operator++() {++(iter1);return *this;}
        iterator<U,W>& operator++(int) {iterator tmp(*this); operator++(); return tmp;}
        bool operator==(const iterator<U,W> other) const {return iter1 == other.iter1;}
@@ -31,8 +31,8 @@ template <typename U, typename W> class iterator {
 public:
 
 zip(T b1, R b2): bin1(b1), bin2(b2){}
-auto begin(){  return iterator<decltype(bin1.begin()),decltype(bin2.begin())>(bin1.begin(), bin2.begin()); }
-auto end(){ return iterator<decltype(bin1.end()),decltype(bin2.end())>(bin1.end(), bin2.end()); }
+auto begin()const {  return iterator<decltype(bin1.begin()),decltype(bin2.begin())>(bin1.begin(), bin2.begin()); }
+auto end()const { return iterator<decltype(bin1.end()),decltype(bin2.end())>(bin1.end(), bin2.end()); }
 
 };
 }
