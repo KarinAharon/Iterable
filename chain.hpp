53854ccb888265b33737048e458f38aa4f9918b2
KarinAharon <44925574+KarinAharon@users.noreply.github.com>
2019-05-24 14:11:23 +0300

    Update chain.hpp

--- chain.hpp
+++ chain.hpp
@@ -15,57 +15,21 @@ const T t_bin;
 
 template <typename U, typename W> class iterator {
 
-private:
-U iter1;
-W iter2;
-bool still_iter1;
+    private:
+        U iter1;
+        W iter2;
 
-public:
+    public:
         
-iterator(U it1 ,W it2):iter1(it1), iter2(it2), still_iter1(true){}
-
-iterator<U,W>& operator++() {
-if (still_iter1) ++(iter1);
-else ++(iter2);
-return *this;
-}
-
-iterator<U,W>& operator++(int) {
-iterator tmp(*this); operator++(); 
-return tmp;
-}
+       iterator(const U &it1 ,const W &it2):iter1(it1), iter2(it2){}
+       iterator<U,W>& operator++() {++(iter1);return *this;}
+       iterator<U,W>& operator++(int) {iterator tmp(*this); operator++(); return tmp;}
+       bool operator==(const iterator<U,W> other) const {return iter1 == other.iter1;}
+       bool operator!=(const iterator<U,W> other) const {return iter1 != other.iter1;}
+       auto &operator*() {return *iter1;}
 
 
-bool operator==( iterator<U,W> other)  {
-if ((still_iter1 == true) && (iter1 == other.iter1))
-still_iter1 = false;
-if (still_iter1==true)
-return (iter1 == other.iter1);
-else
-return (iter2 == other.iter2);
-}
-
-
-bool operator!=( iterator<U,W> other)  {
-if ((still_iter1 == true) && !(iter1 != other.iter1)){
-still_iter1 = false;                                                                                                  
-}
-if (still_iter1==true){
-return (iter1 != other.iter1);
-}
-else{
-return (iter2 != other.iter2);
-}
-}
-
-decltype(*iter1) &operator*() const {
-if (still_iter1)
-return *iter1;
-else 
-return *iter2;
-}
-
-};
+    };
 
 public:
 chain(R r, T t ): r_bin(r), t_bin (t){};
