75def7890a16a5b0fa3f28a17b492d9f944a358b
Oranit Cohen <44542433+Oranit95@users.noreply.github.com>
2019-05-24 13:35:35 +0300

    Update chain.hpp

--- chain.hpp
+++ chain.hpp
@@ -31,9 +31,9 @@ template <typename U, typename W> class iterator {
      //  iterator<U,W>& operator++(int) {iterator tmp(*this); operator++(); return tmp;}
       // bool operator==(const iterator<U,W> other) const {return iter1 == other.iter1;}
        bool operator!=(const iterator<U,W> other) const {
-            if(firstIter && !(it1 != it.it1)) firstIter = false;
-            if(firstIter) return it1 != other.it1;
-            else return it2 != other.it2;
+            if(firstIter && !(iter1 != it.iter1)) firstIter = false;
+            if(firstIter) return iter1 != other.iter1;
+            else return iter2 != other.iter2;
        }
        auto &operator*() const {
            if(firstIter) return *iter1;
